ce83aa42cbd3a3f9e66a151b0a313ae78fb54aef
Denis Vybornyj <denis.vybornyj@gmail.com>
2020-07-29 19:26:32 +0300

    sync workspaces

--- node_modules/src/scripts/helpers/prettyYmdhm.ts
+++
@@ -1,7 +0,0 @@
-type PrettyYmdhm = (date: number | string | Date) => string
-
-export const prettyYmdhm: PrettyYmdhm = date => {
-  const d = new Date(date)
-  const with0 = n => `${n > 9 ? '' : 0}${n}`
-  return `${d.getFullYear()}.${with0(d.getMonth() + 1)}.${with0(d.getDate())} ${with0(d.getHours())}:${with0(d.getMinutes())}`
-}
